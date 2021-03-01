dd13acc205e95063c38afdaab82bebefbc01c3db
Rustam Talapov <rustam_talapov@epam.com>
2021-03-01 08:48:20 +0200

    Project startup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = {
+    term: ''
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="search">
+              Image Search
+            </label>
+            <input
+              type="text"
+              name="search"
+              value={this.state.term}
+              onChange={e => this.setState({
+                term: e.target.value
+              })}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
