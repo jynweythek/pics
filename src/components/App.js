4da394ec4931879f708002cf0a008ddd571d9258
Rustam Talapov <rustam_talapov@epam.com>
2021-03-14 11:54:40 +0200

    unsplash API integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = {
+    images: []
+  };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
+    });
+
+    this.setState({images: response.data.results})
+  };
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '20px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <p>There found {this.state.images.length} images.</p>
       </div>
     )
   }
